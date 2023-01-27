1a77e8ee93a3f6369853d5d979f14d4d70b46e06
André Bianchi <dcbianchi15@gmail.com>
2023-01-27 11:30:25 -0300

    Update _modelophp.php

--- _modelophp.php
+++ _modelophp.php
@@ -4,25 +4,32 @@
   <link rel="stylesheet" href="_css/estilo.css"/>
   <meta charset="UTF-8"/>
   <title>Curso de PHP - CursoemVideo.com</title>
+  <style>
+    h2 {
+      font: 15pt arial;
+      color: #171559;
+      font-weight: bold;
+    }
+  </style>
 </head>
 <body>
 <div>
     <?php
-        $n1 = $_GET["a"];
-        $n2 = $_GET["b"];
+        $n1 = $_GET["x"];
+        $n2 = $_GET["y"];
 
-        echo "Os valores recebidos foram $n1 e $n2";
-        echo "<br> A soma vale ". ($n1+$n2);
-        echo "<br> A subitração vale ".  ($n1-$n2);
-        echo "<br> A Multiplicação vale ". ($n1*$n2);
-        echo "<br> A Divição vale ". ($n1 / $n2);
-        echo "<br> O Modulo é ". ($n1 % $n2);
-        echo "<br> a media vale ". (($n1 + $n2) /2);
+        echo "<h2> Os valores recebidos foram $n1 e $n2</h2>";
 
-        //funções matematicas
-        
+        echo "Absotulo de $n2 ". abs($n2);
+        echo "<br> o valor de $n1 <sup>$n2</sup> é ". pow($n1, $n2);
+        echo "<br> a raiz de $n1 é ". sqrt($n1);
+        echo "<br> O valor de $n2 arredondado é ". floor($n2); 
+                                                  //ceil: sempre arredonda pra cima
+                                                  //Floor: sempre para baixo
+        echo "<br> O valor de $n1 em moeda é R$". number_format($n1, 2, ",",".");
+
+
     ?>
 </div>
 </body>
 </html>
- 
