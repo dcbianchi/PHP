8efaa75ef348807f7b41e84ecfd5e99774539fd3
André Bianchi <dcbianchi15@gmail.com>
2023-01-27 11:14:05 -0300

    Update _modelophp.php

--- _modelophp.php
+++ _modelophp.php
@@ -8,10 +8,19 @@
 <body>
 <div>
     <?php
-        $n1 = 3;
-        $n2 = 2;
-        $s = $n1 + $n2;
-        echo "N1 + N2 = $s";
+        $n1 = $_GET["a"];
+        $n2 = $_GET["b"];
+
+        echo "Os valores recebidos foram $n1 e $n2";
+        echo "<br> A soma vale ". ($n1+$n2);
+        echo "<br> A subitração vale ".  ($n1-$n2);
+        echo "<br> A Multiplicação vale ". ($n1*$n2);
+        echo "<br> A Divição vale ". ($n1 / $n2);
+        echo "<br> O Modulo é ". ($n1 % $n2);
+        echo "<br> a media vale ". (($n1 + $n2) /2);
+
+        //funções matematicas
+        
     ?>
 </div>
 </body>
