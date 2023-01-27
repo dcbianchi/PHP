ad2eaa6436b4e51cee83f759ebc2d18ae71ec09e
André Bianchi <dcbianchi15@gmail.com>
2023-01-27 11:57:45 -0300

    Update _modelophp.php

--- _modelophp.php
+++ _modelophp.php
@@ -15,19 +15,14 @@
 <body>
 <div>
     <?php
-        $n1 = $_GET["x"];
-        $n2 = $_GET["y"];
+      $preco = $_GET["p"];
+      $porc = $_GET["d"];
 
-        echo "<h2> Os valores recebidos foram $n1 e $n2</h2>";
-
-        echo "Absotulo de $n2 ". abs($n2);
-        echo "<br> o valor de $n1 <sup>$n2</sup> é ". pow($n1, $n2);
-        echo "<br> a raiz de $n1 é ". sqrt($n1);
-        echo "<br> O valor de $n2 arredondado é ". floor($n2); 
-                                                  //ceil: sempre arredonda pra cima
-                                                  //Floor: sempre para baixo
-        echo "<br> O valor de $n1 em moeda é R$". number_format($n1, 2, ",",".");
-
+      echo "O preço é $preco e a porcentagem é $porc";
+      echo "<br> O preço do produto é R$". number_format($preco,2,",",".");
+      echo "<br> Com aumento o total fica em R$". number_format($preco + ($preco*$porc/100),2,",",".");
+      echo "<br> Com desconto o total fica em R$". number_format($preco - ($preco*$porc/100),2,",",".");
+      echo "<br> "
 
     ?>
 </div>
