b760047af9059d740d2bf74619791167148f2ec3
André Bianchi <dcbianchi15@gmail.com>
2023-01-27 15:09:19 -0300

    aula 07

--- _modelophp.php
+++ _modelophp.php
@@ -3,7 +3,7 @@
 <head>
   <link rel="stylesheet" href="_css/estilo.css"/>
   <meta charset="UTF-8"/>
-  <title>Curso de PHP - CursoemVideo.com</title>
+  <title>PHP - Aprendendo</title>
   <style>
     h2 {
       font: 15pt arial;
@@ -15,14 +15,11 @@
 <body>
 <div>
     <?php
-      $preco = $_GET["p"];
-      $porc = $_GET["d"];
+      $a = $_GET["a"];
+      $b = $_GET["b"];
+      $maior = $a>$b?$a:$b;
 
-      echo "O preço é $preco e a porcentagem é $porc";
-      echo "<br> O preço do produto é R$". number_format($preco,2,",",".");
-      echo "<br> Com aumento o total fica em R$". number_format($preco + ($preco*$porc/100),2,",",".");
-      echo "<br> Com desconto o total fica em R$". number_format($preco - ($preco*$porc/100),2,",",".");
-      echo "<br> "
+      echo $maior;
 
     ?>
 </div>
