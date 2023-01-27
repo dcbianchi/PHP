04a234905f80e3aa35ad24a01c5398fd086fb6c4
André Bianchi <dcbianchi15@gmail.com>
2023-01-27 10:39:08 -0300

    Update tipos.php

--- Pacote Aula/php-aula04/aula04/tipos.php
+++ Pacote Aula/php-aula04/aula04/tipos.php
@@ -8,6 +8,9 @@
 <body>
 <div>
     <?php
+    //Php é uma linguagem que usa a declaração de variaveis por coersão
+    //Alem disso em PHP as variaveis são iniciadas na sua declaração.
+    //Ou seja, ao declarar a variavel e inserir um valor dentro dela, ela esta iniciada e o tipo de dados é inserido na variel pelo interpretador.
        $nome = "Maria";
        $idade = 18;
        echo "$nome tem $idade anos!";
