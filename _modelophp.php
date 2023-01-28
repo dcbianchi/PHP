0d18946330d67e2930b73dff22b17c6b2f51ae13
André Bianchi <dcbianchi15@gmail.com>
2023-01-28 17:43:01 -0300

    A10-02

--- _modelophp.php
+++ _modelophp.php
@@ -4,28 +4,27 @@
   <link rel="stylesheet" href="_css/estilo.css"/>
   <meta charset="UTF-8"/>
   <title>PHP - Aprendendo</title>
+  
   <?php
-    $n1 = $_GET["n1"]?$_GET["n1"]:"0";
-    $n2 = $_GET["n2"]?$_GET["n2"]:"0";
-    $m = ($n1 + $n2) / 2;
+    $n = $_GET["n"]?$_GET["n"]:"0";
+    $op = $_GET["op"]?$_GET["op"]:"0";
   ?>
+
 </head>
 <body>
 <div>
-    <h2> Aula 9 - Exercicio 03 </h2>
+    <h2> Aula 10 - Exercicio 02 </h2>
     <?php
-      if ($m<5) {
-        $s = "REPROVADO";
-      } elseif ($m>=5 & $m<7) {
-          $s = "em Recuperação";
-        } else {
-          $s = "APROVADO";
-        }
-      echo "A media entre $n1 e $n2 é $m<br>";
-      echo "O aluno esta $s";
+      switch ($op){
+        case 1:
+        break;
+      }
+
+      echo "Você escolheu $op<br>";
+      echo "O resultado da operação é $r";
     ?>
     <br>
-    <button><a href="_modelohtml.html"> Voltar </a></button>
+    <a href="_modelohtml.html" class="botao"> Voltar </a>
 </div>
 </body>
 </html>
