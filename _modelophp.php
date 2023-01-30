90afb934c3a3cdf5e48474fb1ebcc0b8f164e32a
André Bianchi <dcbianchi15@gmail.com>
2023-01-30 15:08:32 -0300

    A14e01

--- _modelophp.php
+++ _modelophp.php
@@ -7,25 +7,10 @@
 </head>
 <body>
 <div>
-    <h2> Aula 13 - Exercicio 02 </h2>
-    <h3>É primo?</h3>
+    <h2> Aula 14 - Exercicio 01 </h2>
+    <h3>Rotinas</h3>
     <?php
-      $n = $_GET["n"]?$_GET["n"]:"1";
-      $m = 0;
-      echo "<strong>$n</strong><br>";
-      echo "Valores Multiplos: ";
-      for ($c=1;$c<=$n;$c++){
-        if ($n%$c==0){
-          $m++;
-          echo "$c ";
-        }
-      }
-      echo "<br> Total de multiplos: $m";
-      if ($m==2){
-        echo "<br> $n É primo";
-      } else { 
-        echo "<br> $n Não é primo";
-      }
+      
     ?>
     <br>
     <br><a href="_modelohtml.html" class="botao"> Voltar </a>
