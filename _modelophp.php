6cabb9819e16ec14eed70a507491cb707066fa59
André Bianchi <dcbianchi15@gmail.com>
2023-01-30 13:25:37 -0300

    A13e03

--- _modelophp.php
+++ _modelophp.php
@@ -4,26 +4,32 @@
   <link rel="stylesheet" href="_css/estilo.css"/>
   <meta charset="UTF-8"/>
   <title>PHP - Aprendendo</title>
-  
-  <?php
-    $n = $_GET["n"]?$_GET["n"]:"1";
-    //$f = $_GET["s"]?$_GET["f"]:"0";
-    //$p = $_GET["p"]?$_GET["p"]:"0";
-  ?>
-
 </head>
 <body>
 <div>
     <h2> Aula 13 - Exercicio 02 </h2>
-    <h3>Taboada |X| </h3>
+    <h3>É primo?</h3>
     <?php
-      for ($c=1;$c<=10;$c++){
-        $t = $n*$c;
-        echo "$n X $c = $t<br>";
+      $n = $_GET["n"]?$_GET["n"]:"1";
+      $m = 0;
+      echo "<strong>$n</strong><br>";
+      echo "Valores Multiplos: ";
+      for ($c=1;$c<=$n;$c++){
+        if ($n%$c==0){
+          $m++;
+          echo "$c ";
+        }
+      }
+      echo "<br> Total de multiplos: $m";
+      if ($m==2){
+        echo "<br> $n É primo";
+      } else { 
+        echo "<br> $n Não é primo";
       }
     ?>
     <br>
     <a href="_modelohtml.html" class="botao"> Voltar </a>
+
 </div>
 </body>
 </html>
