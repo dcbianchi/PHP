1abc3243c76ddef36708c1fb237a06ec72efdd6c
André Bianchi <dcbianchi15@gmail.com>
2023-01-30 10:17:08 -0300

    A11 - 02

--- _modelophp.php
+++ _modelophp.php
@@ -5,36 +5,20 @@
   <meta charset="UTF-8"/>
   <title>PHP - Aprendendo</title>
   
-  <?php
-    $uf = $_GET["uf"]?$_GET["uf"]:"0";
-  ?>
+  <!-- <?php
+ //$ = $_GET["uf"]?$_GET["uf"]:"0";
+  ?> -->
 
 </head>
 <body>
 <div>
-    <h2> Aula 10 - Exercicio 04 </h2>
+    <h2> Aula 11 - Exercicio 02 </h2>
     <?php
-      switch ($uf){
-        case 1:
-          $reg = "Norte";
-          break;
-        case 2:
-          $reg = "Nordeste";
-          break;
-        case 3:
-          $reg = "Centro-Oeste";
-          break;
-        case 4:
-          $reg = "Sudeste";
-          break;
-        case 5:
-          $reg = "Sul";
-          break;
-        default:
-        $reg = "errado";
+      $c = 10;
+      while ($c > 1) {
+        echo "$c ";
+        $c--; //$c = $c-1
       }
-
-      echo "Fica na região $reg";
     ?>
     <br>
     <a href="_modelohtml.html" class="botao"> Voltar </a>
